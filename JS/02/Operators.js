21ab0cd877bdc30b2eb843b50c7ee40d7a625c1c
HPelek <huseyinpelek@gmail.com>
2022-12-13 17:01:25 +0000

    JS

--- JS/02/Operators.js
+++ JS/02/Operators.js
@@ -31,3 +31,76 @@ console.log(isNaN(difference));
 const yearOfBirth = 1990;
 const name = "jane";
 console.log(name + " is " + (2022 - yearOfBirth) + " years old");
+
+console.log(
+  name + " is " + (new Date().getFullYear() - yearOfBirth) + " years old"
+);
+
+console.log(`${name} is ${2022 - yearOfBirth} 
+years old`);
+
+// const pi = 3.14;
+// const r = +prompt("please enter r:");
+// const r = Number(prompt("please enter r:"));
+// console.log(r);
+// const square = pi * r * r;
+// const square = pi * r ** 2;
+
+// console.log(`Square of circle: ${square}`);
+// console.log(`Square of circle: ${square.toFixed(2)}`);
+
+// console.log(`Square of circle: ${Math.floor(square)}`);
+// console.log(`Square of circle: ${Math.ceil(square)}`);
+// console.log(`Square of circle: ${Math.trunc(square)}`);
+// console.log(`Square of circle: ${Math.round(square)}`);
+// console.log(`Random Number ${Math.random(square)}`); //*! Sıfır ile bir arasında bir sayı üretitir */
+
+//?Bazi fonksiyonlar
+// Math.floor();  //* her zaman en yakin alt tamsayiya yuvarlar
+// Math.ceil();  //* her zaman en yakin ust tam tamsayiya yuvarlar
+// Math.trunc(); //* sayinin tam kismini alir.
+// Math.round(); //* en yakin tam sayiya yuvarlar.
+// Math.random(); //* 0 ve 1 arasında rasgele sayi uretir.
+
+const rasgele = Math.round(Math.random() * 100);
+console.log(rasgele);
+
+let a = 5;
+a++;
+console.log(a++);
+y = a + 5;
+console.log(--y);
+
+let z = 4;
+z += 10;
+
+console.log(z);
+let k = 20;
+k -= 10;
+console.log({ k });
+
+let b = 4;
+b *= 3;
+console.log({ b });
+
+// const number = prompt("Please Enter 3 dijit number:");
+
+// const ones = number % 10;
+// const tens = Math.floor(number / 10) % 10;
+// const hundereds = Math.trunc(number / 100);
+// console.log(`ones: ${ones}, tens: ${tens}, hundereds: ${hundereds}`);
+
+const num1 = 3;
+console.log(num1 == 3);
+console.log(num1 === 3);
+console.log(num1 === "3");
+
+const num2 = "3";
+console.log(num1 == num2);
+console.log(num1 != num2);
+
+const num3 = 5;
+const num4 = "1";
+console.log(num1 > num3);
+console.log(num1 <= num3);
+console.log(num2 > num4);
