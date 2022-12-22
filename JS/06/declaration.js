d9dd3ffb2a494c6e6ab92825f3319e4222f73d72
HPelek <huseyinpelek@gmail.com>
2022-12-22 19:06:11 +0000

    func

--- JS/06/declaration.js
+++ JS/06/declaration.js
@@ -1,19 +1,41 @@
 //! Declaration
-function printHello() {
-  console.log("Hello");
-}
+// function printHello() {
+//   console.log("Hello");
+// }
 
-printHello(); //! invoke call
-printHello(); //! invoke call
+// printHello(); //! invoke call
+// printHello(); //! invoke call
 
-function greet(FirstName, Lastname = "") {
-  console.log(`Hi, ${FirstName} ${Lastname}`);
-}
+// function greet(FirstName, Lastname = "") {
+//   console.log(`Hi, ${FirstName} ${Lastname}`);
+// }
 
-greet("huseyin", "pelek");
+// greet("huseyin", "pelek");
 
 // const name = prompt("Your Name");
 // const surname = prompt("Your Surname");
 // greet(name, surname);
 
-greet("pelek");
+// greet("pelek");
+
+// function calculate(name, yob) {
+//   const age = new Date().getFullYear() - yob;
+//   console.log(`${name}'s age is ${age}`);
+
+// }
+
+// function calculate(name, yob) {
+//   const age = new Date().getFullYear() - yob;
+//   return `${name}'s age is ${age}`;
+// }
+// const pelek = calculate("huseyin", 1983);
+// console.log(pelek);
+// console.log(calculate("deli", 1980));
+
+// function oddOrEven(num) {
+//   return num % 2 ? "ODD" : "EVEN";
+// }
+// const number = +prompt("Please enter a number to check odd or even");
+
+// const oddEven = oddOrEven(number);
+// console.log(`${number} is  ${oddEven}`);
