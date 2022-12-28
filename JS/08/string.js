d487092305af2a97bd5807a93a73f8b39418f2b2
HPelek <huseyinpelek@gmail.com>
2022-12-28 17:46:21 +0000

    sstring

---
+++ JS/08/string.js
@@ -0,0 +1,34 @@
+// let str1 = "This is a string";
+// let str2 = "This is a string";
+// let str3 = `This is a string`;
+// let str4 = new String("This is a string");
+
+// console.log(typeof str2);
+// console.log(typeof str3);
+// console.log(typeof str1);
+// console.log(typeof str4);
+
+// console.log(str1.length);
+// console.log(str4);
+
+// let str1 = "Clarusway";
+// let str2 = "Full Stack";
+// let str3 = "";
+// let str4 = str1 + " " + str2;
+// console.log(str1.concat(" ", str2));
+// console.log(str4);
+
+// console.log(str1.length);
+// console.log(str2.length);
+// console.log(str3.length);
+// console.log(str4.length);
+
+let str = "Hello world";
+
+// console.log(str[str.length - 1]);
+// for (i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+for (i = str.length - 1; i >= 0; i--) {
+  console.log(str[i]);
+}
