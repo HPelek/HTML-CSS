f684cf33b99bf1ebebc2fb717a04276433c843a4
HPelek <huseyinpelek@gmail.com>
2023-01-12 17:36:33 +0000

    selectorsDOM

--- JS/13/1-selectors.js
+++ JS/13/1-selectors.js
@@ -57,6 +57,44 @@ console.log(itemList);
 //*              QUERYSELECTOR()
 //* ========================================
 
+const myLi = document.querySelector("li");
+console.log(myLi);
+myLi.style.color = "purple";
+
+const addItem = document.querySelector(".add-item");
+console.log(addItem);
+addItem.style.border = "3px solid grey";
+
+const addBtn = document.querySelector("#btn");
+console.log(addBtn);
+
+addBtn.style.cursor = "pointer";
+
+const itemH2 = document.querySelector(".item-list h2");
+console.log(itemH2);
+
+const addBtnType = document.querySelector("input[type='button']");
+
+console.log(addBtnType);
+console.log(addBtnType.value);
 //* ========================================
 //*              QUERYSELECTORALL()
 //* ========================================
+
+const lists = document.querySelectorAll("ul li");
+console.log(lists);
+
+lists.forEach((li) => console.log(li.innerText));
+
+console.log(
+  document.querySelectorAll("section.item-list ul li:nth-child(4)")[0].innerText
+);
+
+console.log(
+  document.querySelector("section.item-list ul li:nth-child(4)").innerText
+);
+
+console.log(
+  (document.querySelector("section.item-list ul li:nth-child(4)").innerHTML =
+    "<p>Paragraf</p>")
+);
