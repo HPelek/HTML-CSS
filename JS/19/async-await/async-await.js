6ec141d038c4f3e7a037ca7650c6e2763319cb5b
HPelek <huseyinpelek@gmail.com>
2023-01-31 16:55:08 +0000

    async

--- JS/19/async-await/async-await.js
+++ JS/19/async-await/async-await.js
@@ -18,6 +18,50 @@
 //* satirdaki kodun durdurulmasini saglar.
 //* Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi devam eder.
 
-https://newsapi.org/v2/top-headlines?country=us&apiKey=211917e0fcc04a5bbfff6d916a0413ad
+// https://newsapi.org/v2/top-headlines?country=us&apiKey=211917e0fcc04a5bbfff6d916a0413ad
 
-const getNews = async () => {}
+const getNews = async () => {
+  const API_KEY = "211917e0fcc04a5bbfff6d916a0413ad";
+  const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error("Somthing went wrong");
+    }
+    const data = await res.json();
+    renderNews(data.articles);
+  } catch (error) {
+    renderErorr(error);
+  }
+};
+const renderNews = (news) => {
+  console.log(news);
+  const newsDiv = document.getElementById("news");
+
+  news.forEach((item) => {
+    const { title, description, urlToImage, url } = item; //? dest
+    newsDiv.innerHTML += `
+    <div class="col-md-6 col-lg-4 col-xl-3">
+      <div class="card">
+         <img src="${urlToImage}" class="card-img-top" alt="...">
+         <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${description}</p>
+            <a href="${url}" target="_blank" class="btn btn-warning">Detail</a>
+         </div>
+      </div>
+   </div>
+  
+  `;
+  });
+};
+
+const renderErorr = (err) => {
+  console.log(err);
+  const newsDiv = document.getElementById("news");
+  newsDiv.innerHTML = `
+    <h2 class="text-danger">News cannot be fetched </h2>
+    <img src =" ./img/404.png" alt="" </img>`;
+};
+
+getNews();
